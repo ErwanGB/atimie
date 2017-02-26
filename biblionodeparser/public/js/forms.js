d2b3d1f3b18347f9a095f70e34383ef3c504fc43
ErwanGB <erwanguy@gmail.com>
2017-02-26 17:03:47 +0100

    Jquery Edit

--- biblionodeparser/public/js/forms.js
+++ biblionodeparser/public/js/forms.js
@@ -1,7 +1,8 @@
 (function () {
 
   function init(){
-    $('#addSubmitBtn').click(submitButtonHandler);
+    $('#addSubmitBtn').bind("click",submitButtonHandler);
+    $('.editSubmitBtn').bind("click",editButtonHandler);
   }
 
   function submitButtonHandler (evt) {
@@ -37,6 +38,41 @@
     + "<a href='/membre/del?id=" + membre.id + "'" + ">Delete</a></td></tr>"
     ).insertBefore( $( "#addMembreUI" ))
   };
+
+  function editButtonHandler(evt){
+    // Desactiver tous les boutons d'édition
+    $('.editSubmitBtn').unbind("click",editButtonHandler);
+   
+    var first = true;
+     // Selection de tous les td du tr parent du bouton
+    var targets = $(this).parent().parent().children();
+
+    targets.each((index,elmt) =>{
+      // on récupère les noms de classes des td et leur value pour les remplacer par des inputs
+      var className = $(elmt).attr('class')
+      if (className != undefined){
+        var inputValue = $(elmt).html();
+        $(elmt).html("<input type='text' name=" + className + " value=" + inputValue + "></input>");
+      }
+      if(first){$(elmt).focus()};
+      first = false;
+    },getValidButtons(this)
+    );
+  }
+
+  function getValidButtons(place){
+        $(place).html("<a class='editValidBtn'>Valider</a> <a class='editCancelBtn'>Annuler</a>");
+        $('.editValidBtn').bind("click",sendEdit);
+  }
+
+  function sendEdit(){
+    alert("send");
+  }
+
+  function cancelEdit(){
+    alert("cancel");
+  }
+
 
 //init on document ready
 $(document).ready(init);
